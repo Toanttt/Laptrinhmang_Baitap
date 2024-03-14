5611737d6698670394e8c06ca009adcd8a51b973
wuoc <quockhanh4114@gmail.com>
2024-03-14 19:18:40 +0700

    wuoc commit

---
+++ File_Explorer_wuoc/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace File_Explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wBrs = new System.Windows.Forms.WebBrowser();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnFore = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // wBrs
+            // 
+            this.wBrs.Location = new System.Drawing.Point(12, 62);
+            this.wBrs.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wBrs.Name = "wBrs";
+            this.wBrs.Size = new System.Drawing.Size(776, 376);
+            this.wBrs.TabIndex = 0;
+            this.wBrs.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.wBrs_DocumentCompleted);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(12, 12);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(129, 44);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "<<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnFore
+            // 
+            this.btnFore.Location = new System.Drawing.Point(147, 12);
+            this.btnFore.Name = "btnFore";
+            this.btnFore.Size = new System.Drawing.Size(129, 44);
+            this.btnFore.TabIndex = 1;
+            this.btnFore.Text = ">>";
+            this.btnFore.UseVisualStyleBackColor = true;
+            this.btnFore.Click += new System.EventHandler(this.btnFore_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.BackColor = System.Drawing.Color.White;
+            this.txtPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPath.Location = new System.Drawing.Point(343, 10);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(358, 22);
+            this.txtPath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(282, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(707, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(81, 44);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnFore);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.wBrs);
+            this.Name = "Form1";
+            this.Text = "Explorer_Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser wBrs;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnFore;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ File_Explorer_wuoc/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace File_Explorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            wBrs.DocumentCompleted += wBrs_DocumentCompleted;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fdb = new FolderBrowserDialog() { Description = "Select your path." })
+            {
+                if (fdb.ShowDialog() == DialogResult.OK)
+                {
+                    wBrs.Url = new Uri(fdb.SelectedPath);
+                    txtPath.Text = fdb.SelectedPath;
+                }
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (wBrs.CanGoBack)
+            {
+                wBrs.GoBack();
+            }
+        }
+
+        private void btnFore_Click(object sender, EventArgs e)
+        {
+            if (wBrs.CanGoForward)
+            {
+                wBrs.GoForward();
+            }
+        }
+
+        private void wBrs_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (wBrs.Url != null)
+            {
+                txtPath.Text = wBrs.Url.ToString();
+            }
+        }
+    }
+}
