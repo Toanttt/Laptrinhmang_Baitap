46b169df93b0989afb53a4015f70a922803bc455
wuoc <quockhanh4114@gmail.com>
2024-04-12 22:29:57 +0700

    update

---
+++ Lab3_Bai3/Client.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TCP_server
+{
+    partial class test_client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.AutoSize = true;
+            this.btnSend.Enabled = false;
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(588, 163);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(156, 72);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.AutoSize = true;
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(588, 87);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(156, 72);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.AutoSize = true;
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisconnect.Location = new System.Drawing.Point(588, 11);
+            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(156, 72);
+            this.btnDisconnect.TabIndex = 8;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSend.Location = new System.Drawing.Point(12, 12);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(570, 223);
+            this.txtSend.TabIndex = 9;
+            this.txtSend.Text = "";
+            this.txtSend.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSend_KeyDown);
+            // 
+            // test_client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 245);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSend);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "test_client";
+            this.Text = "TCP Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.RichTextBox txtSend;
+    }
+}
---
+++ Lab3_Bai3/Client.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows.Forms;
+using System.Text;
+
+namespace TCP_server
+{
+    public partial class test_client : Form
+    {
+
+        public test_client()
+        {
+            InitializeComponent();
+        }
+
+        private ClientConnection client;
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    client  = new ClientConnection();
+                    client.Connect();
+                    MessageBox.Show("Kết nối đến server thành công");
+                } catch
+                {
+                    MessageBox.Show("Không thành công");
+                    return;
+                }
+
+                BeginInvoke((Action)(() =>
+                {
+                    btnSend.Enabled = true;
+                    btnConnect.Enabled = false;
+                    btnDisconnect.Enabled = true;
+                }));
+            }
+            catch (SocketException ex)
+            {
+                HandleSocketException(ex);
+            }
+        }
+
+        private void HandleSocketException(SocketException ex)
+        {
+            MessageBox.Show($"Không thể kết nối server: {ex.Message}", "Notice");
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (txtSend.Text == "")
+            {
+                return;
+            }
+            string message = txtSend.Text.Trim();
+            client.Send(message);
+            txtSend.Clear();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            BeginInvoke((Action)(() =>
+            {
+                client.Disconnect();
+                btnSend.Enabled = false;
+                btnConnect.Enabled = true;
+                btnDisconnect.Enabled = false;
+            }));
+        }
+
+        private void txtSend_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnSend.PerformClick();
+                txtSend.Focus();
+            }
+        }
+    }
+
+    class ClientConnection
+    {
+        IPAddress ipAdd= IPAddress.Parse("127.0.0.1");
+        IPEndPoint ipEP;
+        TcpClient client;
+        NetworkStream stream;
+
+        public void Connect()
+        {
+            try
+            {
+                client = new TcpClient();
+                ipEP = new IPEndPoint(ipAdd, 8888);
+                client.Connect(ipEP);
+                stream = client.GetStream();
+            } catch
+            {
+                MessageBox.Show("Error connecting to server");
+            }
+        }
+
+        public void Send(string message)
+        {
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Disconnect()
+        {
+            try
+            {
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error closing client socket: " + ex.Message);
+            }
+        }
+    }
+}
